625617e062c26089e7fd3e863494df43100ff7e6
rjrjaleco <33646985+rjrjaleco@users.noreply.github.com>
2017-12-04 10:30:05 +0800

    Add files via upload
    10:30am

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 document.addEventListener("deviceready", on_device_ready, false);
 function on_device_ready()
 {
-	
+	document.getElementById("addExpense").setAttribute("style", "display:none");
 }
 function on_save()
 	{
 		console.log("on_save");
 		var amount = document.getElementById("amount").value;
 		var comment = document.getElementById("comment").value;
-		alert(amount);
+		alert("Saved");
 		alert(comment);
+		document.getElementById("addExpense").setAttribute("style", "display:none");
+		document.getElementById("mainWindow").setAttribute("style", "display:block");
 	}
